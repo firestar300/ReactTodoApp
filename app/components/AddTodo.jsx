813937123dca64934ee590a5d370fed9a39c3144
Milan Ricoul <milan.ricoul@gmail.com>
2017-03-31 23:28:11 +0200

    Create AddTodo Component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var TodoList = require('TodoList');
+
+var AddTodo = React.createClass({
+  onFormSubmit: function(e) {
+    e.preventDefault();
+
+    var text = this.refs.text.value;
+
+    if(text.length > 0) {
+      this.refs.text.value = '';
+      this.props.onAddTodo(text);
+    } else {
+      this.refs.text.focus();
+    }
+  },
+  render: function() {
+    return(
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <input placeholder="What do you need to do ?" ref="text" />
+          <button className="button expanded" type="submit">Add Todo</button>
+        </form>
+      </div>
+    );
+  }
+});
+
+module.exports = AddTodo;
